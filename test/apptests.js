95088fc824a1073de8ebc50dbfc25c846d145862
lrobinsonco <lrobinsonco@gmail.com>
2017-06-04 23:02:35 -0600

    changed theme

--- test/apptests.js
+++ test/apptests.js
@@ -5,6 +5,7 @@
 window.onload = function() {
   var code = $(".codemirror-textarea")[0]
   var editor = CodeMirror.fromTextArea(code, {
+    theme: 'isotope',
     mode: 'javascript',
     lineNumbers : true
   })
